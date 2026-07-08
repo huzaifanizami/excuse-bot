1f78a14505bb3e98419ec6b11af39e1b2cb59c6b
Huzaifa Nizami <136328750+huzaifanizami@users.noreply.github.com>
2026-07-08 15:31:54 -0500

    Add random excuse generator in excuse.js

--- excuse.js
+++ excuse.js
@@ -1,1 +1,4 @@
-
+let excuses = ["sick", "family emergency", "tire burst", "appointment"];
+let randomIndex = Math.floor(Math.random() * excuses.length);
+let message = excuses[randomIndex];
+console.log(message);
